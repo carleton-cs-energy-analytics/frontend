04b90d88acfab7463d6b2ec41eb766967a52f151
Alex T. Davis <git@alextdavis.me>
2019-02-28 21:55:24 -0600

    Fixes for production

--- app.py
+++ app.py
@@ -3,34 +3,33 @@ import flask
 
 BACKEND_URL = os.environ.get('BACKEND_URL') or 'http://energycomps.its.carleton.edu/api/'
 
-app = flask.Flask(__name__, static_folder='static', template_folder='templates')
+application = flask.Flask(__name__, static_folder='static', template_folder='templates')
 
 
-@app.route('/')
+@application.route('/')
 def get_main_page():
     return flask.render_template('search-engine.html', backend_url=BACKEND_URL)
 
 
-@app.route('/points')
+@application.route('/points')
 def get_points_page():
     return flask.render_template('points.html', backend_url=BACKEND_URL)
 
 
-@app.route('/search')
+@application.route('/search')
 def get_search():
     return flask.render_template('search-engine.html', backend_url=BACKEND_URL)
 
 
-@app.route('/rules')
+@application.route('/rules')
 def get_rules_page():
     return flask.render_template('rules.html', backend_url=BACKEND_URL)
 
 
-@app.route('/anomalies')
+@application.route('/anomalies')
 def get_anomaly_page():
     return flask.render_template('anomalies.html', backend_url=BACKEND_URL)
 
+
 if __name__ == '__main__':
-    host = 'localhost'
-    port = '8080'
-    app.run(host=host, port=port)
+    application.run()
