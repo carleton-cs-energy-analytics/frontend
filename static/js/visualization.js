47f5183d943f1ca54a5a32f160f77d1fae77fe4b
tordic <ctordi7545@gmail.com>
2019-03-12 12:54:54 -0500

    apparently we were trying to use the heatmap for floats and ints

--- static/js/visualization.js
+++ static/js/visualization.js
@@ -1,6 +1,6 @@
 function displaySearchResults(values, type) {
     console.log("visualization display results called");
-    if (type === ":type 2 or :type 3") {
+    if (type === ":type 2 or :type 3" || type === ":type 2" || type === ":type 3") {
         d3.selectAll('#visualization svg').remove();
         buildTrendViz([values]);
     } else {
