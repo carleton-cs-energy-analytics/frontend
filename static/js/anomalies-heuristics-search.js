b8ae61607dba8347cf0b633c500b0385ef5d576a
Miguel Alvarez <59747504+alvarezm3@users.noreply.github.com>
2022-02-05 19:50:29 -0600

    Getting building functionality with API

--- static/js/anomalies-heuristics-search.js
+++ static/js/anomalies-heuristics-search.js
@@ -1,4 +1,15 @@
 var BACKEND_URL = "http://energycomps.its.carleton.edu/api/";
+var buildings = new Map([
+    ["Boliou", 1],
+    ["Evans", 2],
+    ["UnID'd Building", 3],
+    ["Hulings", 4],
+    ["Townhouses", 5],
+    ["Weitz", 6],
+    ["Cassat", 7],
+]);
+
+var currentBuilding;
 
 function changeSelection(text) {
     var y = document.getElementsByClassName("btn btn-primary dropdown-toggle");
@@ -59,22 +70,38 @@ function changeBuildingSelection(text) {
     var dropdown = document.getElementsByName("building-button");
 
     var y = dropdown[0].innerHTML = text + ' <span class="caret"></span>';
+
+    $("#building-dropdown li").click(function() {
+        currentBuilding = $(this).text();
+        //alert($(this).text()); // gets text contents of clicked li
+    });
 }
 
 function generateTable() {
-    const url = "http://energycomps.its.carleton.edu/api/measurements";
-    const firstUserNum = document.getElementById('first-num-input').value;
-    const secondUserNum = document.getElementById('second-num-input').value;
-    alert(firstUserNum);
-    alert(secondUserNum);
-    $.getJSON({url: url})
-        .done(function(result, status, xhr) {
-            alert(result);
+    //const url = "http://energycomps.its.carleton.edu/api/rooms?search=@2";
+    const searchURL = "http://energycomps.its.carleton.edu/api/rooms?search=@";
+    const url2 = "http://energycomps.its.carleton.edu/api/points";
+
+    const selectedBuilding = buildings.get(currentBuilding);
+    // alert(selectedBuilding)
+
+    $.getJSON(searchURL + selectedBuilding, function(data) {
+        $.each(JSON.parse(data), function(key, val) {
+            const row = resultsTable.insertRow();
+            const buildingCell = row.insertCell()
+            const building = val.building_name;
+            buildingCell.innerHTML = building;
+            const roomNumCell = row.insertCell();
+            const roomNum = val.room_name;
+            roomNumCell.innerHTML = roomNum;
+            const roomTempCell = row.insertCell();
+            const roomTemp = 50;
+            roomTempCell.innerHTML = roomTemp;
+            const ventPercentCell = row.insertCell();
+            const ventPercent = 70;
+            ventPercentCell.innerHTML = ventPercent;
+
         });
-    // sample api?
-    // $.getJSON({url: BACKEND_URL + firstUserNum})
-    //     .done(function(result, status, xhr) {
-    //         console.log(result);
-    //     });
+    })
+    
     document.getElementById("results-table").style.display="block";
-}
